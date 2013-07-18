b0f00dc7ee646d0abad4bc1ed76b28cfcb3ba5ee
Quinten <quintenpalmer@gmail.com>
2013-07-18 18:36:27 +0000

    Change web port from 8080 to 8888

--- src/qrpgweb/main.go
+++ src/qrpgweb/main.go
@@ -11,7 +11,7 @@ func Main() {
 	http.HandleFunc("/static/",StaticHandler)
 	http.HandleFunc("/play/",PlayHandler)
 	http.HandleFunc("/ajax/",AjaxHandler)
-	port := ":8080"
+	port := ":8888"
 	fmt.Printf("Serving content on %s\n",port)
 	panic(http.ListenAndServe(port, nil))
 }
